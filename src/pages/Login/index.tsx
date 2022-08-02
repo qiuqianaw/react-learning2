da2e049d4331d5af337e8b4140c870c6ed110faf
qiuqianaw <qiuqianaw@163.com>
2022-08-02 14:45:54 +0800

    feature: login page form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,34 @@
+import { Card, Form, Input, Checkbox, Button } from "antd";
+import logo from "@/assets/logo.png";
+import "./index.scss";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <Form initialValues={{ remember: true }}>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
 
 export default Login;
